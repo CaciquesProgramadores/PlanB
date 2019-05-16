90a718a268f8eca82c66e7847f4c6c413cce52da
Jonathan <jonathanolivataiwan@gmail.com>
2019-05-16 15:40:30 +0800

    Error Creating Account

--- app/controllers/accounts.rb
+++ app/controllers/accounts.rb
@@ -28,10 +28,12 @@ module LastWillFile
 
         response.status = 201
         response['Location'] = "#{@account_route}/#{new_account.username}"
+        
         { message: 'Account saved', data: new_account }.to_json
       rescue Sequel::MassAssignmentRestriction
         routing.halt 400, { message: 'Illegal Request' }.to_json
       rescue StandardError => e
+        puts "Error Creating Account"
         puts e.inspect
         routing.halt 500, { message: e.message }.to_json
       end
